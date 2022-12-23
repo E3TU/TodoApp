3ae9568930f5dd88c14249623958ad6774194375
E3TU <eetux22@protonmail.com>
2022-12-23 21:00:05 +0200

    Update
    localstorage works now

---
+++ main.js
@@ -0,0 +1,84 @@
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const taskForm = document.querySelector('#taskform');
+	const input = document.querySelector("#taskinput");
+
+
+	taskForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		const task = {
+			content: input.value,
+		}
+
+		todos.push(task);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		// Reset the form
+		e.target.reset();
+
+		renderTodos()
+	})
+
+	renderTodos()
+})
+
+function renderTodos () {
+	const taskList = document.querySelector('.tasklist');
+	taskList.innerHTML = "";
+	const header = document.createElement("h2");
+	header.innerHTML = "Tasks";
+	taskList.appendChild(header);
+
+	todos.forEach(todo => {
+		const task = document.createElement('div');
+		
+		task.classList.add('task');
+
+		const content = document.createElement('div');
+		const input = document.createElement('input');
+		const buttons = document.createElement('div');
+		const editButton = document.createElement('button');
+		const deleteButton = document.createElement('button');
+		content.appendChild(input);
+
+		content.classList.add('content');
+		buttons.classList.add('buttons');
+		editButton.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input class="text" type="text" value="${todo.content}" readonly>`;
+		editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+		deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+		buttons.appendChild(editButton);
+		buttons.appendChild(deleteButton);
+
+		content.appendChild(buttons);
+		task.appendChild(content);
+		taskList.appendChild(task);
+		
+		editButton.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			editButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				renderTodos()
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			header.innerHTML = "";
+			renderTodos()
+		})
+
+	})
+}
